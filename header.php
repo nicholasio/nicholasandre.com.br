d12a5d566bab3b4dad31727e311bb3cb1a670e5a
Nícholas André <nicholas@iotecnologia.com.br>
2013-12-26 18:24:19 -0300

    Enviando html/css base

---
+++ header.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Faculty-Responsive university staff personal page</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="author" content="owwwlab.com">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        
+        <meta name="description" content="A theme for faculty profile page" />
+        <meta name="keywords" content="faculty profile, theme,css, html, jquery, transition, transform, 3d, css3" />
+
+        <link rel="shortcut icon" href="../favicon.ico">
+
+        <!--CSS styles-->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">  
+        <link rel="stylesheet" href="css/perfect-scrollbar-0.4.5.min.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link id="theme-style" rel="stylesheet" href="css/styles/default.css">
+
+        
+        <!--/CSS styles-->
+        <!--Javascript files-->
+        <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
+        <script type="text/javascript" src="js/TweenMax.min.js"></script>
+        <script type="text/javascript" src="js/jquery.touchSwipe.min.js"></script>
+        <script type="text/javascript" src="js/jquery.carouFredSel-6.2.1-packed.js"></script>
+        
+        <script type="text/javascript" src="js/modernizr.custom.63321.js"></script>
+        <script type="text/javascript" src="js/jquery.dropdownit.js"></script>
+
+        <script type="text/javascript" src="js/jquery.stellar.min.js"></script>
+        <script type="text/javascript" src="js/ScrollToPlugin.min.js"></script>
+
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+
+        <script type="text/javascript" src="js/masonry.min.js"></script>
+
+        <script type="text/javascript" src="js/perfect-scrollbar-0.4.5.with-mousewheel.min.js"></script>
+
+        <script type="text/javascript" src="js/magnific-popup.js"></script>
+        <script type="text/javascript" src="js/custom.js"></script>
+
+        <!--/Javascript files-->
+
+    </head>
+    <body>
+
+        <div id="wrapper">
+            <a href="#sidebar" class="mobilemenu"><i class="icon-reorder"></i></a>
+
+            <div id="sidebar">
+                <div id="main-nav">
+                    <div id="nav-container">
+                        <div id="profile" class="clearfix">
+                            <div class="portrate hidden-xs"></div>
+                            <div class="title">
+                                <h2>Nícholas André</h2>
+                                <h3>Programador Web</h3>
+                            </div>
+                            
+                        </div>
+                        <ul id="navigation">
+                            <li class="currentmenu">
+                              <a href="index.html">
+                                <div class="icon icon-user"></div>
+                                <div class="text">Sobre mim</div>
+                              </a>
+                            </li>  
+                            
+                            <li>
+                              <a href="research.html">
+                                <div class="icon icon-book"></div>
+                                <div class="text">Pesquisas</div>
+                              </a>
+                            </li> 
+                            
+                            <li>
+                              <a href="publication.html">
+                                <div class="icon icon-edit"></div>
+                                <div class="text">Publicações</div>
+                              </a>
+                            </li> 
+
+                            <li>
+                              <a href="teaching.html">
+                                <div class="icon icon-time"></div>
+                                <div class="text">Ensino</div>
+                              </a>
+                            </li>
+
+                            <li>
+                              <a href="gallery.html">
+                                <div class="icon icon-picture"></div>
+                                <div class="text">Galeria</div>
+                              </a>
+                            </li>
+
+                            <li>
+                              <a href="contact.html">
+                                  <div class="icon icon-calendar"></div>
+                                  <div class="text">Contact & Meet Me</div>
+                              </a>
+                            </li>
+
+                            <li>
+                              <a href="cv.pdf">
+                                  <div class="icon icon-download-alt"></div>
+                                  <div class="text">Download CV</div>
+                              </a>
+                            </li>
+                        </ul>
+                    </div>        
+                </div>
+                
+                <div class="social-icons">
+                    <ul>
+                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                        <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                    </ul>
+                </div>    
+            </div>
---
+++ index.php
@@ -0,0 +1,214 @@
+<?php include("header.php"); ?>
+<div id="main">
+    <div id="biography" class="page home" data-pos="home">
+        <div class="pageheader">
+            <div class="headercontent">
+                <div class="section-container">
+                    
+                    <div class="row">
+                        <div class="col-sm-2 visible-sm"></div>
+                        <div class="col-sm-8 col-md-4">
+                            <div class="biothumb">
+                                <img alt="image" src="img/personal/personal-big.png" class="img-responsive">
+
+                            </div>
+                            
+                        </div>
+                        <div class="clearfix visible-sm visible-xs"></div>
+                        <div class="col-sm-12 col-md-7">
+                            <h3 class="title">Nícholas André</h3>
+                            <p>Um programador</p>
+                        </div>  
+                        
+                    </div>
+                </div>        
+            </div>
+        </div>
+
+        <div class="pagecontents">
+            <div class="section color-1">
+                <div class="section-container">
+                    <div class="row">
+                        <div class="col-md-5 col-md-offset-1">
+                            <div class="title text-center">
+                                <h3>Academic Positions</h3>
+                            </div>
+                            <ul class="ul-dates">
+                                <li>
+                                    <div class="dates">
+                                        <span>Present</span>
+                                        <span>2005</span>
+                                    </div>
+                                    <div class="content">
+                                        <h4>General Atlantic Professor</h4>
+                                        <p><em>Stanford University</em>, Graduate School of Business</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dates">
+                                        <span>2005</span>
+                                        <span>2004</span>
+                                    </div>
+                                    <div class="content">
+                                        <h4>Full Professor</h4>
+                                        <p><em>Stanford University</em>, Graduate School of Business</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dates">
+                                        <span>2004</span>
+                                        <span>2001</span>
+                                    </div>
+                                    <div class="content">
+                                        <h4>Assistant Professor</h4>
+                                        <p><em>UCLA</em>, Anderson Graduate School of Management</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dates">
+                                        <span>2001</span>
+                                        <span>1999</span>
+                                    </div>
+                                    <div class="content">
+                                        <h4>Visiting Assistant Professor</h4>
+                                        <p><em>Columbia University</em>, Graduate School of Business</p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="title text-center">
+                                <h3>Education & Training</h3>
+                            </div>
+                            <ul class="ul-card">
+                                <li>
+                                    <div class="dy">
+                                        <span class="degree">Ph.D.</span>
+                                        <span class="year">1995</span>
+                                    </div>
+                                    <div class="description">
+                                        <p class="waht">Ph.D. in Marketing</p>
+                                        <p class="where">Stanford University Graduate School of Business</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dy">
+                                        <span class="degree">M.B.A.</span><span class="year">1993</span>
+                                    </div>
+                                    <div class="description">
+                                        <p class="waht">Master of Business Administration</p>
+                                        <p class="where">Boston University</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dy">
+                                        <span class="degree">B.A.</span><span class="year">1989</span>
+                                    </div>
+                                    <div class="description">
+                                        <p class="waht">Bachelor of Arts in Psychology</p>
+                                        <p class="where">University of California, Berkeley</p>
+                                    </div>
+                                </li>
+                                
+                            </ul>
+
+                        </div>    
+                    </div>    
+                </div>
+                    
+            </div>
+
+            <div class="section color-2">
+                <div class="section-container">
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <div class="title text-center">
+                                <h3>Honors, Awards and Grants</h3>
+                            </div>
+                            <ul class="timeline">
+                                        
+                                <li class="open">
+                                    <div class="date">SCP 2014</div>
+                                    <div class="circle"></div>
+                                    <div class="data">
+                                        <div class="subject">Distinguished Scientific Achievement Award</div>
+                                        <div class="text row">
+                                            <div class="col-md-2">
+                                                <img alt="image" class="thumbnail img-responsive" src="img/personal/awards100x100.png" >
+                                            </div>
+                                            <div class="col-md-10">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas dapibus lectus non dignissim. Pellentesque auctor ornare urna, volutpat condimentum quam porttitor at. Vestibulum tincidunt diam in eros aliquam luctus. Donec sagittis a purus a porttitor. Sed non feugiat enim. Donec eget metus erat. Vivamus sed consequat orci. Aenean commodo lectus sed purus auctor ullamcorper.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="date">2012-2013</div>
+                                    <div class="circle"></div>
+                                    <div class="data">
+                                        <div class="subject">Ormond Family Faculty Fellow</div>
+                                        <div class="text row">
+                                            <div class="col-md-2">
+                                                <img alt="image" class="thumbnail img-responsive" src="img/personal/awards100x100.png" >
+                                            </div>
+                                            <div class="col-md-10">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas dapibus lectus non dignissim. Pellentesque auctor ornare urna, volutpat condimentum quam porttitor at. Vestibulum tincidunt diam in eros aliquam luctus. Donec sagittis a purus a porttitor. Sed non feugiat enim. Donec eget metus erat. Vivamus sed consequat orci. Aenean commodo lectus sed purus auctor ullamcorper.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="date">June 2011</div>
+                                    <div class="circle"></div>
+                                    <div class="data">
+                                        <div class="subject">Nautilus Silver Award for Dragonfly Effect</div>
+                                        <div class="text row">
+                                            <div class="col-md-2">
+                                                <img alt="image" class="thumbnail img-responsive" src="img/personal/awards100x100.png" >
+                                            </div>
+                                            <div class="col-md-10">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas dapibus lectus non dignissim. Pellentesque auctor ornare urna, volutpat condimentum quam porttitor at. Vestibulum tincidunt diam in eros aliquam luctus. Donec sagittis a purus a porttitor. Sed non feugiat enim. Donec eget metus erat. Vivamus sed consequat orci. Aenean commodo lectus sed purus auctor ullamcorper.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="date">2000 - 2003</div>
+                                    <div class="circle"></div>
+                                    <div class="data">
+                                        <div class="subject">Hong Kong Science International Research Grant</div>
+                                        <div class="text row">
+                                            <div class="col-md-2">
+                                                <img alt="image" class="thumbnail img-responsive" src="img/personal/awards100x100.png" >
+                                            </div>
+                                            <div class="col-md-10">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas dapibus lectus non dignissim. Pellentesque auctor ornare urna, volutpat condimentum quam porttitor at. Vestibulum tincidunt diam in eros aliquam luctus. Donec sagittis a purus a porttitor. Sed non feugiat enim. Donec eget metus erat. Vivamus sed consequat orci. Aenean commodo lectus sed purus auctor ullamcorper.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="date">1999</div>
+                                    <div class="circle"></div>
+                                    <div class="data">
+                                        <div class="subject">Citibank Best Teacher Award (school-wide award, UCLA)</div>
+                                        <div class="text row">
+                                            <div class="col-md-2">
+                                                <img alt="image" class="thumbnail img-responsive" src="img/personal/awards100x100.png" >
+                                            </div>
+                                            <div class="col-md-10">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas dapibus lectus non dignissim. Pellentesque auctor ornare urna, volutpat condimentum quam porttitor at. Vestibulum tincidunt diam in eros aliquam luctus. Donec sagittis a purus a porttitor. Sed non feugiat enim. Donec eget metus erat. Vivamus sed consequat orci. Aenean commodo lectus sed purus auctor ullamcorper.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>                            
+        </div>
+    </div>
+</div>
+<?php include("footer.php"); ?>
