4a618cc4ccdb8ced3348412c39d22b97eab85418
Nícholas André <nicholas@iotecnologia.com.br>
2014-01-11 12:22:47 -0300

    Atualizando

---
+++ portfolio.php
@@ -0,0 +1,121 @@
+<?php 
+    $_page = "portfolio";
+    include("header.php");
+?>
+
+            <div id="main">
+            
+                <div id="gallery" class="page">
+                    <div class="pagecontents">
+                        
+                        <div class="section pageheader" id="gallery-header">
+                            <div class="section-container">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <h2>Portfólio</h2>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p>Seleciono aqui alguns dos trabalhos em que desenvolvi para diversas empresas e clientes.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="section color-1" id="gallery-large">
+                            <div class="section-container">
+                                
+                                <ul id="grid" class="grid">
+                                    <li>
+                                        <div>
+                                            <img alt="image" src="img/gallery/450x600.png">
+                                            <a href="img/gallery/450x600.png" class="popup-with-move-anim">
+                                                <div class="over">
+                                                    <div class="comein">
+                                                        <i class="icon-search"></i>
+                                                        <div class="comein-bg"></div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <img alt="image" src="img/gallery/600x600.png">
+                                            <a href="img/gallery/600x500.png" class="popup-with-move-anim">
+                                                <div class="over">
+                                                    <div class="comein">
+                                                        <h3>Image Title</h3>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                        <div class="comein-bg"></div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <img alt="image" src="img/gallery/900x600.png">
+                                            <a href="img/gallery/900x600.png" class="popup-with-move-anim">
+                                                <div class="over">
+                                                    <div class="comein">
+                                                        <i class="icon-search"></i>
+                                                        <div class="comein-bg"></div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <img alt="image" src="img/gallery/400x300.png">
+                                            <a href="img/gallery/400x300.png" class="popup-with-move-anim"> 
+                                                <div class="over">
+                                                    <div class="comein">
+                                                        <h3>Image Title</h3>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                        <div class="comein-bg"></div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <img alt="image" src="img/gallery/400x300.png">
+                                            <a href="img/gallery/400x300.png" class="popup-with-move-anim">
+                                                <div class="over">
+                                                    <div class="comein">
+                                                        <i class="icon-search"></i>
+                                                        <div class="comein-bg"></div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <img alt="image" src="img/gallery/800x600.png">
+                                            <a href="img/gallery/800x600.png" class="popup-with-move-anim">
+                                                <div class="over">
+                                                    <div class="comein">
+                                                        <h3>Image Title</h3>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                        <div class="comein-bg"></div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </li>
+                                    
+                                    
+                                </ul>
+                                    
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+<?php include("footer.php");?>
